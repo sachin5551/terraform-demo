a0719160f254467564e8b723d07db01ffafc3fba
root <root@S-5CG141C14J.localdomain>
2026-05-10 13:32:44 +0530

    Terraform CodePipeline demo

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+    region   = "ap-south-1"
+    profile  = "terraform"
+}
+
+data "aws_ami" "amazon_linux" {
+    most_recent = true
+
+    owners = ["amazon"]
+
+    filter {
+        name   = "name"
+        values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    }
+}
+
+resource "aws_instance" "my_ec2" {
+    ami            = data.aws_ami.amazon_linux.id
+    instance_type  = "t3.micro"
+
+    tags = {
+      Name = "Terraform-EC2"
+    }
+}
